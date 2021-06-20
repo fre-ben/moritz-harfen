b72bacd5f1023618c80ab5b66bf85c1cd8771464
fre-ben <fschocke@gmail.com>
2021-06-20 14:34:26 +0200

    ✨ Add LanguageSelector component (WIP, not functional)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import Logo from "../components/navbar/Logo";
 import NavLink from "../components/navbar/NavLink";
 import NavLinkDropdown from "../components/navbar/NavLinkDropdown";
+import LanguageSelector from "../components/navbar/LanguageSelector";
 
 const IndexPage = () => {
   return (
@@ -11,6 +12,8 @@ const IndexPage = () => {
       <NavLink text="Tolle Sachen" link="/" />
       <NavLink text="Werkstatt" link="/" />
       <NavLink text="Zubehör" link="/" />
+      <LanguageSelector />
+
       <p className="font-bold text-2xl block my-4">
         Test Das ist ein toller Text, der hoffentlich richtig dargestellt wird
       </p>
