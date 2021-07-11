c949a185c69d5f48d30c6b010a523140e6c582e9
fre-ben <fschocke@gmail.com>
2021-07-11 18:00:04 +0200

    ✨ Add stories

--- src/components/navbar/Navbar.stories.tsx
+++ src/components/navbar/Navbar.stories.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { ComponentStory, Meta } from "@storybook/react";
 import NavLink from "./NavLink";
+import NavLinkDropdown from "./NavLinkDropdown";
+import Logo from "./Logo";
+import LanguageSelector from "./LanguageSelector";
+import Navbar from "./Navbar";
 
 export default {
   title: "Components/Navbar",
@@ -15,10 +19,36 @@ export default {
   argTypes: {},
 } as Meta;
 
-const Template: ComponentStory<typeof NavLink> = args => <NavLink {...args} />;
+const Template1: ComponentStory<typeof NavLink> = args => <NavLink {...args} />;
+const Template2: ComponentStory<typeof NavLinkDropdown> = args => (
+  <NavLinkDropdown {...args} />
+);
+const Template3: ComponentStory<typeof Logo> = args => <Logo {...args} />;
+const Template4: ComponentStory<typeof LanguageSelector> = args => (
+  <LanguageSelector {...args} />
+);
+const Template5: ComponentStory<typeof Navbar> = args => <Navbar {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const Link = Template1.bind({});
+Link.args = {
+  text: "Leihinstrumente",
+  link: "/",
+};
+
+export const LinkDropdown = Template2.bind({});
+LinkDropdown.args = {
   text: "Harfen",
   link: "/",
 };
+
+export const logo = Template3.bind({});
+logo.args = {
+  size: "4xl",
+  mr: "2",
+};
+
+export const LangSelector = Template4.bind({});
+LangSelector.args = {};
+
+export const Bar = Template5.bind({});
+Bar.args = {};
