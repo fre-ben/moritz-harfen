c08a6428661f0ee929cd5c21c5478009f17dfa40
fre-ben <fschocke@gmail.com>
2021-07-17 13:49:48 +0200

    ♻️ Refactor ButtonLink styles Remove duplicate classes

--- src/components/button/ButtonLink.tsx
+++ src/components/button/ButtonLink.tsx
@@ -8,15 +8,18 @@ export type ButtonLinkProps = {
 };
 
 function ButtonLink({ destination, label, primary }: ButtonLinkProps) {
-  const primaryStyle =
-    "border-4 border-highlight border-opacity-60 rounded-lg px-4 py-1 hover:border-opacity-100 focus:border-opacity-100 transition ease-in-out duration-300";
-
-  const secondaryStyle =
-    "border-4 border-black border-opacity-70 rounded-lg bg-black text-white bg-opacity-70 px-4 py-1 hover:bg-opacity-100 focus:bg-opacity-100 transition ease-in-out duration-300";
+  const styles = {
+    primary:
+      "border-highlight hover:border-opacity-100 focus:border-opacity-100",
+    secondary:
+      "border-black bg-black text-white bg-opacity-70 hover:bg-opacity-100 focus:bg-opacity-100",
+  };
 
   return (
     <Link
-      className={primary ? primaryStyle : secondaryStyle}
+      className={`border-4 rounded-lg border-opacity-60 px-4 py-1 transition ease-in-out duration-300 ${
+        primary ? styles.primary : styles.secondary
+      }`}
       to={`/${destination}`}
     >
       {label}
