43605d18a80b55a545f8912673812ecf3f9b30cd
fre-ben <fschocke@gmail.com>
2021-07-17 13:25:58 +0200

    ✨ Add ButtonLink components to index For testing purposes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 import Navbar from "../components/navbar/Navbar";
 import Layout from "../components/Layout";
+import ButtonLink from "../components/button/ButtonLink";
 
 const IndexPage = () => {
   return (
     <Layout>
       <Navbar />
       <h1>Hello World</h1>
+      <ButtonLink destination="Test" label="Test" primary={true} />
+      <ButtonLink destination="Kontakt" label="Kontakt" primary={false} />
     </Layout>
   );
 };
