6a8d9fdf7bd9147bae443fd835c3f0b4aa4c7805
fre-ben <fschocke@gmail.com>
2021-08-17 21:10:29 +0200

    💄 Fix gap-issues Using padding, instead of gap/margin resolves background issues

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,20 +5,29 @@ import NavLink from "./NavLink";
 import BurgermenuButton from "../button/BurgermenuButton";
 
 const Navbar = () => {
-  return (
-    <nav className="inline-flex items-center sticky top-0 pt-10 z-50 bg-background w-full justify-center p-full">
-      <div className="visible md:hidden w-full grid grid-flow-col items-center">
-        <Logo mr="40" size="" />
-        <BurgermenuButton />
-      </div>
-      <div className="hidden md:block">
-        <Logo mr="40" size="4" />
+  const NavItems = () => {
+    return (
+      <>
         <NavLinkDropdown text="harfen" link="/" />
         <NavLink text="leihharfen" link="/" />
         <NavLink text="zubehör" link="/" />
         <NavLink text="werkstatt" link="/" />
         <NavLink text="vita" link="/" />
         <NavLink text="kontakt" link="/" />
+      </>
+    );
+  };
+
+  return (
+    <nav className="inline-flex items-center sticky top-0 pt-10 z-50 bg-background w-full justify-center p-full">
+      <div className="visible md:hidden w-full grid grid-flow-col items-center">
+        <Logo mr="40" size="" />
+        <BurgermenuButton />
+        {/* Add Dropdown for NavLink Options, dropping down when clicking Burgermenu */}
+      </div>
+      <div className="hidden md:block">
+        <Logo mr="40" size="4" />
+        {NavItems()}
       </div>
     </nav>
   );
