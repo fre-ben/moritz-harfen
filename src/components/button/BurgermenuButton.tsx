c324a94c8114ddd6ab3002fc0b5a46d666ed1c67
fre-ben <fschocke@gmail.com>
2021-08-17 20:33:56 +0200

    💄 Add onClick  Icon change Done by changing the SVGs path

--- src/components/button/BurgermenuButton.tsx
+++ src/components/button/BurgermenuButton.tsx
@@ -1,6 +1,18 @@
 import * as React from "react";
+import { useState } from "react";
 
 function BurgermenuButton() {
+  //
+  const [path, setPath] = useState("M4 6h16M4 12h16M4 18h16");
+
+  function changePath() {
+    if (path === "M4 6h16M4 12h16M4 18h16") {
+      setPath("M6 18L18 6M6 6l12 12");
+    } else if (path === "M6 18L18 6M6 6l12 12") {
+      setPath("M4 6h16M4 12h16M4 18h16");
+    }
+  }
+
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -8,12 +20,13 @@ function BurgermenuButton() {
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
+      onClick={() => changePath()}
     >
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth={2}
-        d="M4 6h16M4 12h16M4 18h16"
+        d={path}
       />
     </svg>
   );
