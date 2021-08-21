08102aa1c79db31b9a8c98e380e5f452337f9a64
fre-ben <fschocke@gmail.com>
2021-08-21 19:28:46 +0200

    🎨 Edit Link syntax in NavLink to match buttonLink syntax

--- src/components/navbar/NavLink.tsx
+++ src/components/navbar/NavLink.tsx
@@ -9,7 +9,7 @@ type NavLinkProps = {
 const NavLink = ({ text, link }: NavLinkProps) => {
   return (
     <p className="inline text-lg md:mx-2 lg:mx-7 border-b-2 border-opacity-0 border-highlight hover:border-opacity-100 transition ease-in-out duration-400">
-      <Link to={link}>{text}</Link>
+      <Link to={`/${link}`}>{text}</Link>
     </p>
   );
 };
