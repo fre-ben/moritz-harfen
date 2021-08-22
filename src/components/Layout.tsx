0e820cd053eb640944434c5dda5da8cd6dfff659
fre-ben <fschocke@gmail.com>
2021-08-22 15:00:37 +0200

    ✨ Add Footer to Layout component Footer will be displayed on every page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { ReactChild } from "react";
+import Footer from "./footer/Footer";
 
 type LayoutProps = {
   children: ReactChild[] | ReactChild;
@@ -9,6 +10,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="grid auto-rows-min place-items-center px-5 md:px-10 min-h-screen min-w-screen bg-background">
       {children}
+      <Footer />
     </div>
   );
 };
