85c0c203cec0e9e56157c7d9bef00547c30dd201
fre-ben <fschocke@gmail.com>
2021-08-23 23:00:01 +0200

    ✨ Pass flow prop to existing Content components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const IndexPage = () => {
   return (
     <Layout>
       <Navbar />
-      <Content>
+      <Content flow="col">
         <div className="px-5">
           <h1 className="text-5xl mb-10 font-medium">moritz harfen</h1>
           <p className="text-lg mb-5 w-full">
