55628458c16c1834a71a5cb744f7a7647d1f0ff5
fre-ben <fschocke@gmail.com>
2021-09-15 21:26:14 +0200

    💬 Update text on werkstatt page

--- src/pages/werkstatt.tsx
+++ src/pages/werkstatt.tsx
@@ -16,23 +16,24 @@ function Werkstatt() {
             Handarbeit. Er entwarf eigene Modelle und entwickelte sie über die
             Jahre in Zusammenarbeit mit Musiker:innen immer weiter. Mehrere
             hundert Instrumente sind in dieser Werkstatt, die Anfang der 2000er
-            Jahre ins nahegelegene Haunetal umzog, entstanden. Am 4.4.2018
-            verstarb Albrecht und eine Nachfolge war zunächst ungewiss.
+            Jahre ins nahegelegene Haunetal umzog, entstanden. Dabei wurde er
+            von seiner Frau Claudia Uecker unterstützt.
           </p>
           <p className="mb-4">
-            Im Jahr 2020 wurde die Werkstatt von dem Instrumentenbauer Moritz
-            Dortmund übernommen. Moritz hat das Schreinerhandwerk in
+            Am 4.4.2018 verstarb Albrecht und eine Nachfolge war zunächst
+            ungewiss. Im Jahr 2020 wurde die Werkstatt von dem Instrumentenbauer
+            Moritz Dortmund übernommen. Moritz hat das Schreinerhandwerk in
             Berchtesgaden und anschließend das Instrumentenbauhandwerk in
             Mittenwald gelernt und war dreieinhalb Jahre als Wandergeselle auf
             traditioneller Wanderschaft. Auf der Wanderschaft lernte Moritz das
             &quot;Atelier für keltische Harfen&quot; kennen und entschloss sich
-            dazu, die Werkstatt zu übernehmen. Claudia Uecker, Albrechts Frau,
-            führt dabei das Leihharfengeschäft weiter. So wird Albrecht Nüchters
-            Fackel weitergetragen und es entstehen in dieser Werkstatt weiterhin
-            hochwertige, handwerklich gefertigte Harfen.
+            dazu, die Werkstatt zu übernehmen. Claudia führt das
+            Leihharfengeschäft weiter.
           </p>
           <p className="mb-4">
-            Die Instrumente werden ausschließlich aus heimischen Hölzern
+            So wird Albrecht Nüchters Fackel weitergetragen und es entstehen in
+            dieser Werkstatt weiterhin hochwertige, handwerklich gefertigte
+            Harfen. Die Instrumente werden ausschließlich aus heimischen Hölzern
             gefertigt. Neben der hohen Qualität der Harfen spielt auch
             nachhaltiges Wirtschaften stets eine große Rolle. Es werden nicht
             nur die Harfen stetig weiterentwickelt, sondern auch die Produktion
