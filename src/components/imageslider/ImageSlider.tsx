b7d649e751370fa307cc64933ada3067600e0b0d
fre-ben <fschocke@gmail.com>
2021-09-16 22:35:55 +0200

    💄 Add custom styles for ImageSlider Change thumbnail border color

--- src/components/imageslider/ImageSlider.tsx
+++ src/components/imageslider/ImageSlider.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import ImageGallery, { ReactImageGalleryItem } from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
+import "../../css/ImageSlider.css";
 
 export type ImageSliderProps = {
   images?: ReactImageGalleryItem[];
