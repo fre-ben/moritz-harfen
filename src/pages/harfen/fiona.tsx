4327ea52f3eb1172198076114611789d3250166c
fre-ben <fschocke@gmail.com>
2021-09-21 21:49:04 +0200

    ✨ Add Fiona harfen page

---
+++ src/pages/harfen/fiona.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import Content from "../../components/Content";
+import Layout from "../../components/Layout";
+import Navbar from "../../components/navbar/Navbar";
+
+function Fiona() {
+  return (
+    <Layout>
+      <Navbar />
+      <Content
+        className="lg:grid-flow-col"
+        flow="col"
+        headline="fiona (36 Saiten)"
+      >
+        <p>Fotos der Harfe folgen bald.</p>
+        <div className="md:w-7/12 lg:w-10/12">
+          <p className="mb-5 lg:w-9/12 text-lg">
+            Mit 36 Saiten und einer Größe von etwa 140 cm ist das Modell
+            &quot;Fiona&quot; das größte in der Reihe.
+            <br />
+            Voller Klang und große Tragweite machen es zu einem hervorragenden
+            Konzertinstrument.
+          </p>
+          <table className="text-lg">
+            <tr>
+              <td>Saiten:</td>
+              <td className="pl-3">36 (a&lsquo;&lsquo;&lsquo;-A1)</td>
+            </tr>
+            <tr>
+              <td>Höhe:</td>
+              <td className="pl-3">ca. 140cm</td>
+            </tr>
+            <tr>
+              <td>Gewicht:</td>
+              <td className="pl-3">ca. 10 kg</td>
+            </tr>
+          </table>
+        </div>
+      </Content>
+    </Layout>
+  );
+}
+
+export default Fiona;
