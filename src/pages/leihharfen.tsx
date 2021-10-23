8ea4839335716de718fa3f50bf48180a71f8b9ea
fre-ben <fschocke@gmail.com>
2021-10-23 13:34:23 +0200

    ✨ Add Leihharfen image to Leihharfen page

--- src/pages/leihharfen.tsx
+++ src/pages/leihharfen.tsx
@@ -3,10 +3,19 @@ import Navbar from "../components/navbar/Navbar";
 import Layout from "../components/Layout";
 import ButtonLink from "../components/button/ButtonLink";
 import Content from "../components/Content";
-// import { StaticImage } from "gatsby-plugin-image";
 import { Helmet } from "react-helmet";
+import ImageSlider from "../components/imageslider/ImageSlider";
+import { ReactImageGalleryItem } from "react-image-gallery";
+import leihharfen from "../images/harfen/leihharfen.jpg";
 
 const Leihharfen = () => {
+  const leihharfenImages: ReactImageGalleryItem[] = [
+    {
+      original: leihharfen,
+      originalClass: "cursor-default",
+    },
+  ];
+
   return (
     <Layout>
       <Helmet
@@ -22,7 +31,7 @@ const Leihharfen = () => {
       />
       <Navbar />
       <Content flow="col" headline="leihharfen">
-        <div className="lg:pl-20 md:w-7/12 lg:w-9/12">
+        <div className="lg:pl-20 md:w-11/12 lg:w-10/12 order-2 lg:order-1">
           <p className="mb-5 w-11/12">
             Sie möchten eine Harfe leihen? Bei uns finden Sie eine große Auswahl
             an Harfen in den unterschiedlichsten Größen und Ausführungen. <br />
@@ -32,15 +41,8 @@ const Leihharfen = () => {
           <p className="mb-10">Anfragen bitte telefonisch oder per E-Mail.</p>
           <ButtonLink destination="kontakt" label="Kontakt" primary={true} />
         </div>
-        <div className="pr-10">
-          {/* no image available at the moment */}
-          {/* <StaticImage
-            src="../images/placeholder_dude.jpg"
-            alt="placeholder image"
-            placeholder="blurred"
-            width={400}
-            height={500}
-          /> */}
+        <div className="lg:pr-20 sm:pl-0 order-1 lg:order-2">
+          <ImageSlider images={leihharfenImages} />
         </div>
       </Content>
     </Layout>
