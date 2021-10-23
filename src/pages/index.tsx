c0021a91c0043424f3a4592144174824ce25b9e3
fre-ben <fschocke@gmail.com>
2021-10-23 11:40:29 +0200

    ✨ Add new logo and landingpage design to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import Navbar from "../components/navbar/Navbar";
 import Layout from "../components/Layout";
 import ButtonLink from "../components/button/ButtonLink";
 import Content from "../components/Content";
-import { StaticImage } from "gatsby-plugin-image";
 import LeaderDisclaimer from "../components/leaderDisclaimer/LeaderDisclaimer";
 import { Helmet } from "react-helmet";
+import newLogo from "../images/new_logo.svg";
 
 const IndexPage = () => {
   return (
@@ -23,20 +23,21 @@ const IndexPage = () => {
       />
       <Navbar />
       <Content flow="row" headline="">
-        <StaticImage
-          src="../images/old_logo.jpg"
-          alt="placeholder image"
-          placeholder="blurred"
-          layout="fixed"
-          className="rounded-xl mx-5 lg:mx-0"
-        />
-        <div className="grid px-5 justify-items-start sm:justify-items-center border-highlight border-b border-opacity-50 pb-20">
-          <p className="text-xl md:text-3xl mb-5 w-full">
+        <div
+          className="grid px-0 sm:px-5 justify-items-start sm:justify-items-center pb-16 pt-16 lg:pb-28 lg:pt-28"
+          style={{
+            backgroundImage: `linear-gradient(rgba(230, 230, 230, 0.8), rgba(230, 230, 230, 0.8)), url(${newLogo})`,
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain",
+          }}
+        >
+          <p className="text-xl md:text-3xl mb-5 w-full text-left sm:text-center">
             Willkommen auf der Seite der Harfenbauwerkstatt Moritz Dortmund
             <br />– ehemals <strong>„Atelier für keltische Harfen“</strong> von
             Albrecht Nüchter.
           </p>
-          <div className="grid grid-flow-col auto-cols-min place-items-center gap-x-20">
+          <div className="grid grid-flow-col auto-cols-min place-items-center gap-x-20 lg:gap-x-40">
             <ButtonLink destination="harfen" label="Harfen" primary={true} />
             <ButtonLink destination="kontakt" label="Kontakt" primary={true} />
           </div>
