151d84cbd447291a1d2f8d53a7f5733004f74033
Freben <fschocke@gmail.com>
2022-03-17 23:34:47 +0100

    Remove placeholder images from ImageSlider component

--- src/components/imageslider/ImageSlider.tsx
+++ src/components/imageslider/ImageSlider.tsx
@@ -4,28 +4,13 @@ import "react-image-gallery/styles/css/image-gallery.css";
 import "../../css/ImageSlider.css";
 
 export type ImageSliderProps = {
-  images?: ReactImageGalleryItem[];
+  images: ReactImageGalleryItem[];
 };
 
 function ImageSlider({ images }: ImageSliderProps) {
-  const placeholderImages = [
-    {
-      original: "https://picsum.photos/id/1018/1000/600/",
-      thumbnail: "https://picsum.photos/id/1018/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1015/1000/600/",
-      thumbnail: "https://picsum.photos/id/1015/250/150/",
-    },
-    {
-      original: "https://picsum.photos/id/1019/1000/600/",
-      thumbnail: "https://picsum.photos/id/1019/250/150/",
-    },
-  ];
-
   return (
     <ImageGallery
-      items={images ? images : placeholderImages}
+      items={images}
       showPlayButton={false}
       showNav={true}
       showBullets={true}
