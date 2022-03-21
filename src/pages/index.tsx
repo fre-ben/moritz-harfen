aa9babd583f64ddba112f450973dc74abb0ea734
Frederik <77615388+fre-ben@users.noreply.github.com>
2022-03-21 19:45:15 +0100

    ✨ Add Instagram Link to index page (#75)
    * ✨ Add Instagram Link to index page
    
    * Center Instagram component
    
    * Center ButtonLinks on index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Content from "../components/Content";
 import LeaderDisclaimer from "../components/leaderDisclaimer/LeaderDisclaimer";
 import { Helmet } from "react-helmet";
 import newLogo from "../images/new_logo.svg";
+import Instagram from "../components/social/Instagram";
 
 const IndexPage = () => {
   return (
@@ -24,7 +25,7 @@ const IndexPage = () => {
       <Navbar />
       <Content flow="row" headline="">
         <div
-          className="grid px-0 sm:px-5 justify-items-start sm:justify-items-center pb-16 pt-16 lg:pb-28 lg:pt-28"
+          className="grid px-0 sm:px-5 justify-items-center pb-16 pt-16 lg:pb-28 lg:pt-28"
           style={{
             backgroundImage: `linear-gradient(rgba(230, 230, 230, 0.8), rgba(230, 230, 230, 0.8)), url(${newLogo})`,
             backgroundPosition: "center",
@@ -37,11 +38,12 @@ const IndexPage = () => {
             <br />– ehemals <strong>„Atelier für keltische Harfen“</strong> von
             Albrecht Nüchter.
           </p>
-          <div className="grid grid-flow-col auto-cols-min place-items-center gap-x-20 lg:gap-x-40">
+          <div className="grid grid-flow-col auto-cols-min place-items-center gap-x-16 sm:gap-x-20 lg:gap-x-40">
             <ButtonLink destination="harfen" label="Harfen" primary={true} />
             <ButtonLink destination="kontakt" label="Kontakt" primary={true} />
           </div>
         </div>
+        <Instagram />
         <LeaderDisclaimer />
       </Content>
     </Layout>
