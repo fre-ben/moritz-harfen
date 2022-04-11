00e76ec4c73e3ba74e0f3ec0f0cc3931aac69cc0
Freben <fschocke@gmail.com>
2022-04-11 22:27:04 +0200

    Add Todo-comment

--- src/components/navbar/LanguageSelector.tsx
+++ src/components/navbar/LanguageSelector.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { changeLocale } from "gatsby-plugin-react-intl";
 
+//TODO: Add layout/styles for this (once Moritz has translations)
 const LanguageSelector = () => {
   return (
     <div className="inline text-right whitespace-nowrap lg:mr-3 mt-5">
